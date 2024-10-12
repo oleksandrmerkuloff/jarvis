f02df4915e76bdccc8c141d13ddbc19391cd28ba
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-10-12 16:12:20 +0300

    create load/save to json methods, write some bugfixes and start creating run method

--- jarvis.py
+++ jarvis.py
@@ -8,7 +8,25 @@ BASE_DIR = os.path.dirname(__file__)
 
 class Jarvis:
     def __init__(self) -> None:
-        self.tasks = {}
+        self.tasks = self.get_tasks_data()
+
+    def create_data_file(self):
+        with open(BASE_DIR + '/task.json', 'w') as data_file:
+            json.dump({}, data_file)
+
+    def get_tasks_data(self):
+        if not os.path.exists(BASE_DIR + '/task.json'):
+            self.create_data_file()
+
+        with open(BASE_DIR + '/task.json', 'r') as data_file:
+            return json.load(data_file)
+
+    def save_tasks(self):
+        with open(BASE_DIR + '/task.json', 'w') as data_file:
+            json.dump(self.tasks, data_file)
+
+    def get_current_date(self):
+        return datetime.now().strftime('%Y:%m:%d %H:%M')
 
     def collect_task_data(self):
         title = input("Enter task title: ")
@@ -20,8 +38,8 @@ class Jarvis:
             'description': description,
             'status': status,
             'theme': theme,
-            'created_date': datetime.now().strftime('%Y:%m:%d %H:%M'),
-            'updated_date': datetime.now().strftime('%Y:%m:%d %H:%M')
+            'created_date': self.get_current_date(),
+            'updated_date': self.get_current_date()
         }
         return data
 
@@ -33,6 +51,8 @@ class Jarvis:
             task_id = 1
         self.tasks[task_id] = task_data
 
+        self.save_tasks()
+
     def update_task(self, task_id):
         print('Columns: title, description, status, theme')
         to_update = input('Enter what column whould you like to update: ').lower()
@@ -43,14 +63,19 @@ class Jarvis:
 
         new_data = input('Write new data for this column: ')
         self.tasks[task_id][to_update] = new_data
-        return True
+        self.tasks[task_id]['updated_date'] = self.get_current_date()
+
+        self.save_tasks()
+
+    def run(self):
+        print('Greetings, sir. My name is Jarvis.')
+        print('Type commands for work: <commands list>')
+        while True:
+            user_request = input('print here: ')
+            if user_request == 'create':
+                self.create_task()
 
 
 if __name__ == "__main__":
     jar = Jarvis()
-    jar.create_task()
-    print(jar.tasks)
-    jar.create_task()
-    print(jar.tasks)
-    jar.update_task(2)
-    print(jar.tasks)
+    jar.run()
