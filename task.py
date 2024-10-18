e42e182c806fa31f8b4a47ff56c5b20ce5d0d1f1
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-10-18 23:29:40 +0300

    start creating task class and add basic obj attr

---
+++ task.py
@@ -0,0 +1,9 @@
+class Task:
+    def __init__(self) -> None:
+        self.title = None
+        self.description = None
+        self.status = None
+        self.theme = None
+        self.created_date = None
+        self.updated_date = None
+        self.is_visible = True
