53cbd2bf4db5d5731e7fa86a008d057d51ca84fa
Oleksandr Merkulov <oleksandrmerkuloff@gmail.com>
2024-10-18 23:32:06 +0300

    add methods

--- task.py
+++ task.py
@@ -7,3 +7,18 @@ class Task:
         self.created_date = None
         self.updated_date = None
         self.is_visible = True
+
+    def set_title(self):
+        pass
+
+    def set_description(self):
+        pass
+
+    def set_status(self):
+        pass
+
+    def set_theme(self):
+        pass
+
+    def change_visible(self):
+        pass
